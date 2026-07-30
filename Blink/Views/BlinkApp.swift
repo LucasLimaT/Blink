4c773d08008c0b0e68a1f53b5ca4ab87d7fcf1cf
Lucas Taveira <140752422+LucasLimaT@users.noreply.github.com>
2026-07-30 19:25:04 -0400

    chore: base struct

---
+++ Blink/Views/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by Turma02-20 on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
